a0edfe00f6ebb5ac6cc6bb039aa78577cc78de95
Farenza <farenza@localhost.localdomain>
2018-03-04 01:33:13 +0700

    Adding layout

---
+++ app/src/main/java/com/farenza/dompet/MainActivity.java
@@ -0,0 +1,13 @@
+package com.farenza.dompet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
